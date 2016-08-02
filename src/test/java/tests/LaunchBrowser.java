f6533dd889ec4376404b3cd3ac83d0a38e79b0c0
mansi910 <mansi09101993@gmail.com>
2016-08-02 11:31:58 +0530

    Add files via upload

---
+++ src/test/java/tests/LaunchBrowser.java
@@ -0,0 +1,38 @@
+package tests;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+
+public class LaunchBrowser {
+	private static WebDriver driver;
+	
+	@Test
+	public void executeTest() {
+		
+		try {
+			DesiredCapabilities capability;
+			capability = DesiredCapabilities.chrome();
+			capability.setBrowserName("chrome");
+			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\c5245770\\Downloads\\chromedriver.exe");
+			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);	
+			//driver = new ChromeDriver();
+		  	driver.get("https://google.com");
+		 	driver.manage().window().maximize();
+		 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 			 		
+		 	driver.quit();
+
+		 	
+		} catch (Exception e) {
+			 e.printStackTrace();
+		}
+	 	
+	 
+	}
+
+}
